686f76beccf1aff84c4d39bfdc9b38fc075d85e3
3uclid3 <3uclid3@nubwork.dev>
2025-10-16 22:10:25 -0400

    fix(xmake): move set_runtimes inside platform check for Windows

--- xmake.lua
+++ xmake.lua
@@ -3,11 +3,13 @@ set_version("v0.1.0")
 set_license("GPL-3.0-or-later")
 
 set_languages("c++23")
+set_warnings("allextra", "error")
 
 set_toolchains("llvm")
-set_runtimes("MT")
 
-set_warnings("allextra", "error")
+if is_plat("windows") then
+    set_runtimes("MT")
+end
 
 add_rules("mode.debug", "mode.release", "mode.coverage")
 add_rules("plugin.compile_commands.autoupdate", { outputdir = "$(builddir)" })
